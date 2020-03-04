7f28a771cedf4e768a05a3851ba4d52a956f0dea
Greg Pailet <gpailet@mit.edu>
2020-03-03 19:41:02 -0500

    Player uses wsad to move, arrows to shoot, can toggle fire between automatic or not

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,7 +8,10 @@ public class Mover : MonoBehaviour
     private Rigidbody2D rb2d;
     private Vector2 moveVector;
     public int speed;
+    public int revSpeed;
+    public int angularOffset;
     public GameObject projectile;
+    public bool isAutomatic;
 
     public float coolDown;
     private float coolDownInterval;
@@ -19,11 +22,23 @@ public class Mover : MonoBehaviour
         coolDownInterval = coolDown;
     }
 
+    private Vector2 GetFireDirection(int angularOffset)
+	{
+        float rotateAngle = angularOffset + rb2d.rotation;
+
+        float x = Mathf.Cos(rotateAngle * Mathf.Deg2Rad);
+        float y = Mathf.Sin(rotateAngle * Mathf.Deg2Rad);
+        Vector2 rotateVector = new Vector2(x, y);
+
+        return rotateVector;
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
 
         moveVector = new Vector2(0, 0);
+        int rotateDirection = 0;
 
         if (Input.GetKey("w"))
             moveVector += Vector2.up;
@@ -34,17 +49,26 @@ public class Mover : MonoBehaviour
         if (Input.GetKey("d"))
             moveVector += Vector2.right;
 
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rotateDirection += 1;
+        if (Input.GetKey(KeyCode.RightArrow))
+            rotateDirection += -1;
+
         moveVector.Normalize();
 
         rb2d.MovePosition(rb2d.position + speed * moveVector * Time.fixedDeltaTime);
+        rb2d.MoveRotation(rb2d.rotation + rotateDirection * revSpeed * Time.fixedDeltaTime);
+
+        Vector2 fireDirection = GetFireDirection(angularOffset);
+
 
         coolDownInterval -= Time.fixedDeltaTime;
 
-        if (coolDownInterval <= 0 && Input.GetKey("space") && moveVector.sqrMagnitude > 0)
+        if (coolDownInterval <= 0 && (isAutomatic || Input.GetKey("space")))
         {
             coolDownInterval = coolDown;
-            GameObject bullet = Instantiate(projectile, transform.position + (Vector3)moveVector, Quaternion.identity) as GameObject;
-            bullet.GetComponent<Rigidbody2D>().AddForce(moveVector * 1000);
+            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody2D>().AddForce(fireDirection * 1000);
         }
 
 
