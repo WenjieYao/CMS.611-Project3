17a77cb8b443649bc992af7bc2dfa9de68cff234
Greg Pailet <gpailet@mit.edu>
2020-03-03 20:01:44 -0500

    Add basic monster spawner and script

---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+
+    public float spawnTime;
+    public GameObject monsterPrefab;
+
+
+    private float spawnTimeRemaining;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        spawnTimeRemaining -= Time.fixedDeltaTime;
+        if (spawnTimeRemaining <= 0)
+        {
+            GameObject monster = Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
